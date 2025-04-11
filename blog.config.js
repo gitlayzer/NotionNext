bd5b988df7a36e7552b58482a1606b69a787dd52
Gitlayzer <18037803502@163.com>
2025-04-11 18:57:47 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Gitlayzer', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的运维开发仔子🍚', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的运维开发仔🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.devops-engineer.com.cn', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '云原生,服务网格,Gitlayzer 的博客,运维开发工程师,Kuhernetes,DevOps,Service Mesh,Golang 开发', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
